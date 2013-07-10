d254ac69c3795095ece7a6803950b70f5e81b0b4
Sorin Dabiste <sorin0187@gmail.com>
2013-07-10 10:48:02 +0000

    fix encoding bug

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     fs.readFileSync('index.html', function(err, data) {
 	if (err) throw err;
-	response.send(data.toString())
+	response.send(data.toString('utf8'));
     });
 });
 
